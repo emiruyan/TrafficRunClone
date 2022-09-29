44f5088b404d516f6f6f6533ef734f2d24053a0b
emiruyan <emir-uyn@hotmail.com>
2022-09-29 20:28:30 +0300

    New Enemies

--- TrafficRunClone/Assets/Scripts/EnemyCar/SpawnController.cs
+++ TrafficRunClone/Assets/Scripts/EnemyCar/SpawnController.cs
@@ -1,27 +1,47 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class SpawnController : MonoBehaviour
 {
-   [SerializeField] private GameObject enemyCarPrefab;
+   // [SerializeField] private GameObject enemyCarPrefab;
    [SerializeField] private float minZ;
    [SerializeField] private float maxZ;
    [SerializeField] private float x;
    [SerializeField] private float interval;
+   //
+   //
+   // private void Start()
+   // { 
+   //    InvokeRepeating("CarSpawn", interval, interval);
+   // }
+   //
+   // private void CarSpawn()
+   // {
+   //    GameObject instance = Instantiate(enemyCarPrefab);
+   //    instance.transform.position = new Vector3(x, 1, Random.Range(minZ,maxZ));
+   // }
+
+
+   [SerializeField] private GameObject[] enemyCarPrefab;
 
 
    private void Start()
-   { 
-      InvokeRepeating("CarSpawn", interval, interval);
+   {
+      InvokeRepeating("InstantiateObject", interval, interval);
    }
 
-   private void CarSpawn()
+   private void InstantiateObject()
    {
-      GameObject instance = Instantiate(enemyCarPrefab);
-      instance.transform.position = new Vector3(x, 1, Random.Range(minZ,maxZ));
+
+      for (int i = 0; i < 1; i++)
+      {
+         GameObject newEnemyCar = Instantiate(enemyCarPrefab[Random.Range(0,enemyCarPrefab.Length)]);
+         newEnemyCar.transform.position = new Vector3(x,1, Random.Range(minZ, maxZ));
+      }
    }
 
 
